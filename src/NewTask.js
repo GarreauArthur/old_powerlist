5a2ed54f078411f74acb05bd8a76e084fe5cde72
cqncpdp <arthurgarreau.web@gmail.com>
2019-08-02 16:28:37 +0200

    Introduce shitty and ugly template to create a new task
    It seems like since the introduction of hooks in react, nobody is using classes.
    So let create function components now.

---
+++ src/NewTask.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  TextField, Paper, Checkbox, Button,
+  FormControlLabel, FormGroup
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles'
+import SplitButton from './SplitButton'
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    margin: 10,
+  }
+}));
+
+
+
+export default function NewTask(props)
+{
+
+    const classes = useStyles();
+
+    return (
+      <Paper>
+        <FormGroup className={classes.form}>
+          <TextField
+            id="outlined-name"
+            label="New task"
+            margin="normal"
+            variant="outlined"
+            autoComplete="off"
+          />
+          <FormControlLabel control={
+            <Checkbox name="repeat" value="repeat" color="primary" />
+            }
+            label="Repeat every day"
+          />
+          <SplitButton />
+          <Button variant="contained" color="primary" fullWidth={true}>
+            Create new task
+          </Button>
+        </FormGroup>
+      </Paper>
+    );
+
+}
+//        <div className={classes.form}></div>
